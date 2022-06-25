071de48f3502658f6baf2346dd1d2794a3fc70c0
yobilyo <bbb.yousfi@gmail.com>
2022-06-25 16:23:28 +0200

    write in HTML from another HTML file custom made code that uses the ` to skip lines without breaking the js method write.
    cannot be used on this project because the navbar link references always change based on the current document path

---
+++ js/writeNavbarInHTML.js
@@ -0,0 +1,35 @@
+writeNavbarInHTML();
+
+function writeNavbarInHTML () {
+    document.write(`
+    <nav>
+    <div>
+        <a href="index.html">Accueil</a>
+    </div>
+    <div>
+        Consoles
+        <a href="consoles/pc.html">PC</a>
+        <a href="consoles/ps4.html">PS4</a>
+        <a href="consoles/switch.html">Switch</a>
+    </div>
+    <div>
+        A propos               
+        <a href="a_propos/histoire.html">Histoire</a>
+        <a href="a_propos/equipe.html">L'équipe</a>
+        <a href="a_propos/prix.html">Prix et récompenses</a>
+    </div>
+    <div>
+        Store               
+        <a href="store/goodies.html">Goodies</a>
+        <a href="store/events.html">Events</a>
+    </div>
+    <div>
+        Community              
+        <a href="community/news.html">News</a>
+        <a href="community/tests.html">Tests</a>
+        <a href="community/avis.html">Avis</a>
+    </div>
+    <div>
+        <a href="contact.html">Contact</a>
+    </div>`);
+}
